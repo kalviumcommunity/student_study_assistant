f01108a94758e8adc22972a97b880eaedfb6918c
bhagirathauti <bhagirath961778@gmail.com>
2025-08-29 13:30:18 +0530

    zeroshot

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const {GoogleGenerativeAI} = require("@google/generative-ai");
-const runSystemUser = require("./prompts/systemUser.js");
+// const runSystemUser = require("./prompts/systemUser.js");
+const runZeroShot = require("./prompts/zeroShot.js");
+
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-runSystemUser(genAI);
+// runSystemUser(genAI);
+runZeroShot(genAI);
